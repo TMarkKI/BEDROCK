12105b02731259afa0d0074717b490a513467db8
Mark Tan <117105171+TMarkKI@users.noreply.github.com>
2026-01-26 13:15:12 +0000

    Repack figure2.py
    Used to plot mod composition (failed/mod/canonical/variant), and visualised the percentages of each mod composition in a given sample across the genome.

---
+++ figures/figure2.py
@@ -0,0 +1,76 @@
+##bases composition (failed/mod/canonical/variant)
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from utils import make_palette
+from config import BASE_CALL_PALETTE
+
+
+def base_composition(df, threshold):
+    df = df[df.mod_score >= threshold]
+
+    summary = (
+        df.groupby(["Chromosome", "sample"])
+        [["mod", "canonical_mod", "other_mod",
+          "delete_mod", "fail_mod", "variant_mod", "no_call"]]
+        .sum()
+        .reset_index()
+    )
+
+    long = summary.melt(
+        id_vars=["Chromosome", "sample"],
+        var_name="base_call_type",
+        value_name="n_sites"
+    )
+
+    long["percent"] = (
+        long.groupby(["Chromosome", "sample"])
+            .n_sites.transform(lambda x: x / x.sum() * 100)
+    )
+
+    long["threshold"] = f">={threshold}x"
+    return long
+
+def plot_base_composition(df, outdir):
+    order = [
+        "canonical_mod",
+        "mod",
+        "variant_mod",
+        "other_mod",
+        "fail_mod",
+        "delete_mod",
+        "no_call",
+    ]
+
+    df["base_call_type"] = pd.Categorical(
+        df["base_call_type"],
+        categories=order,
+        ordered=True
+    )        
+    
+    base_palette = make_palette(
+        df["base_call_type"].unique(),
+        palette=BASE_CALL_PALETTE
+    )
+
+
+
+    g = sns.catplot(
+        data=df,
+        x="Chromosome",
+        y="percent",
+        hue="base_call_type",
+        row="threshold",
+        col="sample",
+        kind="bar",
+        multiple="stack",
+        height=4,
+        palette=base_palette
+    )
+    g.set_xticklabels(rotation=45)
+    g.set_axis_labels("Chromosome", "Percentage of Bases Called")
+    
+    plt.tight_layout()
+    g.savefig(f"{outdir}/figure2a.tiff", dpi=300)
+    plt.close()
