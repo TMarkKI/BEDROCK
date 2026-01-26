b2dc990ba0260213197b4fd878ceb0f9e38fa9fc
Mark Tan <117105171+TMarkKI@users.noreply.github.com>
2026-01-26 13:17:57 +0000

    Repack figure3cd.py
    Plot modifications across the genome based on the number of total bases in 1000bp.

---
+++ figures/figure3cd.py
@@ -0,0 +1,105 @@
+#modified bases by AT or CG in 1kb
+import pandas as pd
+import pyranges as pr
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from utils import make_palette
+
+WINDOW_SIZE = 1000
+
+
+def make_windows(df_windows):
+    
+    assert {"Chromosome", "start", "end"}.issubset(df_windows.columns)
+    
+    return pr.PyRanges(
+        df_windows.rename(
+            columns={"start": "Start", "end": "End"}
+        )
+        
+    )
+
+
+def bed_to_ranges(df):
+    return pr.PyRanges(
+        df.rename(
+            columns={
+                "Chromosome": "Chromosome",
+                "Start_chrom_pos": "Start",
+                "End_chrom_pos": "End",
+            }
+        )
+    )
+
+
+def summarize_modifications(
+    bed_df,
+    window_df,
+    mod_codes,
+):
+    bed = bed_to_ranges(bed_df)
+    windows = make_windows(window_df)
+
+    overlaps = bed.join(windows)
+
+    df = overlaps.df
+    df = df[df["mod_code"].isin(mod_codes) & (df["mod"] == 1)]
+
+    summary = (
+        df.groupby(
+            ["Chromosome", "start", "end", "strand", "sample"]
+        )
+        .size()
+        .reset_index(name="mod")
+    )
+
+    summary.loc[summary.strand == "-", "mod"] *= -1
+
+    return summary
+
+
+def plot_mod_windows(df, outpath, ylab):
+    
+    df["strand"] = pd.Categorical(
+        df["strand"],
+        categories=["+", "-"],
+        ordered=True
+    )
+
+    chrom_order = list(dict.fromkeys(df["Chromosome"]))
+
+    df["Chromosome"] = pd.Categorical(
+        df["Chromosome"],
+        categories=chrom_order,
+        ordered=True
+    )
+
+    palette = make_palette(df["strand"].unique(), palette="Set1")
+
+    g = sns.FacetGrid(
+        df,
+        row="sample",
+        col="Chromosome",
+        sharex=False,
+        height=3,
+    )
+
+    g.map_dataframe(
+        sns.barplot,
+        x="start",
+        y="mod",
+        hue="strand",
+        palette=palette,
+        dodge=False,
+    )
+
+    g.add_legend()
+    g.set_axis_labels("Genomic position (bp)", ylab)
+
+    for ax in g.axes.flatten():
+        ax.tick_params(axis="x", labelbottom=False)
+
+    plt.tight_layout()
+    plt.savefig(outpath, dpi=300)
+    plt.close()
