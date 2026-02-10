021d8f93d57c0adf1f458467b2d61fac5ef89854
Mark Tan <117105171+TMarkKI@users.noreply.github.com>
2026-02-10 16:13:03 +0000

    Update merge_figure3.py
    Forgot to call in bed_to_ranges; causes keyerror with start vs start_chrom_pos

--- BEDROCK_main/figures/merge_figure3.py
+++ BEDROCK_main/figures/merge_figure3.py
@@ -1,5 +1,5 @@
 from figures.figure3ab import window_base_counts, plot_window_counts
-from figures.figure3cd import summarize_modifications, plot_mod_windows
+from figures.figure3cd import bed_to_range, summarize_modifications, plot_mod_windows
 
 
 def run_figure3(ref_fasta, bed_all, chr_map, outdir):
@@ -12,8 +12,10 @@ def run_figure3(ref_fasta, bed_all, chr_map, outdir):
     plot_window_counts(at_df, outdir / "figure3b.tiff", "AT Count")
 
     # ---- FIGURE 3C/D ----
-    cg_mods = summarize_modifications(bed_all, cg_df, mod_codes=["h", "m"])
+    bed_ranges = bed_to_ranges(bed_all)
+
+    cg_mods = summarize_modifications(bed_ranges, cg_df, mod_codes=["h", "m"])
     plot_mod_windows(cg_mods, outdir / "figure3c.tiff", "Modified Cytosine Count")
 
-    at_mods = summarize_modifications(bed_all, at_df, mod_codes=["a"])
+    at_mods = summarize_modifications(bed_ranges, at_df, mod_codes=["a"])
     plot_mod_windows(at_mods, outdir / "figure3d.tiff", "Modified Adenosine Count")
