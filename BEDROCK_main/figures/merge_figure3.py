6af9db474f8d6b4334841017c40ac1d02adf3fca
Mark Tan <117105171+TMarkKI@users.noreply.github.com>
2026-02-10 19:34:02 +0000

    Update merge_figure3.py

--- BEDROCK_main/figures/merge_figure3.py
+++ BEDROCK_main/figures/merge_figure3.py
@@ -1,5 +1,5 @@
 from figures.figure3ab import window_base_counts, plot_window_counts
-from figures.figure3cd import bed_to_ranges, summarize_modifications, plot_mod_windows
+from figures.figure3cd import summarize_modifications, plot_mod_windows
 
 
 def run_figure3(ref_fasta, bed_all, chr_map, outdir):
@@ -12,8 +12,6 @@ def run_figure3(ref_fasta, bed_all, chr_map, outdir):
     plot_window_counts(at_df, outdir / "figure3b.tiff", "AT Count")
 
     # ---- FIGURE 3C/D ----
-    bed_ranges = bed_to_ranges(bed_all)
-
     cg_mods = summarize_modifications(bed_ranges, cg_df, mod_codes=["h", "m"])
     plot_mod_windows(cg_mods, outdir / "figure3c.tiff", "Modified Cytosine Count")
 
