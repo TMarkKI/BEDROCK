790e01a76c0445dfef2540e2b6939609a674d87c
Mark Tan <117105171+TMarkKI@users.noreply.github.com>
2026-02-11 20:35:07 +0000

    Update figure3cd.py

--- BEDROCK_main/figures/figure3cd.py
+++ BEDROCK_main/figures/figure3cd.py
@@ -94,6 +94,7 @@ def plot_mod_windows(df, outpath, ylab):
         dodge=False,
     )
 
+    g.add_legend()
     g.legend.set_bbox_to_anchor(1, 1)
     g.legend.set_loc("center left")
     g.set_axis_labels("Genomic position (bp)", ylab)
