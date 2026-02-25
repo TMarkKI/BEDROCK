8ce56ad6b97ca2ff1aba05256516692d57493f5a
Mark Tan <117105171+TMarkKI@users.noreply.github.com>
2026-02-25 21:03:11 +0000

    Update figure3cd.py

--- BEDROCK_main/figures/figure3cd.py
+++ BEDROCK_main/figures/figure3cd.py
@@ -8,27 +8,31 @@ from utils import make_palette
 
 WINDOW_SIZE = 1000
 
-def overlap_with_windows(df_bedmethyl, df_windows):
+def make_windows(df_windows):
+    return pr.PyRanges(
+        df_windows.rename(columns={
+            "Start": "Start",
+            "End": "End"
+        })
+    )
 
-    win = df_windows.rename(columns={"Start": "Start", "End": "End"})
-    bed = df_bedmethyl.rename(columns={
-        "Start_chrom_pos": "Start",
-        "End_chrom_pos": "End",
-        "sample" : "sample"
-    })
-    
-    win_pr = pr.PyRanges(win)
-    bed_pr = pr.PyRanges(bed)
-    overlaps = bed_pr.join(win_pr)
+def bed_to_ranges(df_bed):
+    return pr.PyRanges(
+        df_bed.rename(columns-{
+            "Start_chrom_pos": "Start",
+            "End_chrom_pos": "End"
+        })
+    )
 
-    return overlaps.df
+def summarize_modifications(bed_df, window_df, mod_codes):
+    bed = bed_to_ranges(bed_df)
+    windows = make_windows(window_df)
 
-def summarize_cg(overlap_df):
-
-    df = overlap_df.copy()
+    overlaps = bed.join(windows)
+    df = overlaps.df
 
     df = df[
-        df["mod_code"].isin(["h", "m"]) &
+        df["mod_code"].isin(mod_codes) &
         (df["mod"] == 1)
     ]
 
@@ -45,29 +49,7 @@ def summarize_cg(overlap_df):
 
     return summary
 
-def summarize_at(overlap_df):
-
-    df = overlap_df.copy()
-
-    df = df[
-        df["mod_code"].isin(["a"]) &
-        (df["mod"] == 1)
-    ]
-
-    summary = (
-        df.groupby(
-            ["Chromosome", "Start", "End", "strand", "sample"],
-            as_index=False
-        )
-        .size()
-        .rename(columns={"size": "mod"})
-    )
-
-    summary.loc[summary["strand"] == "-", "mod"] *= -1
-
-    return summary
-
-def plot_windows(df, outpath, ylab):
+def plot_mod_windows(df, outpath, ylab):
 
     df = df.copy()
 
@@ -87,29 +69,29 @@ def plot_windows(df, outpath, ylab):
         height=3
     )
 
-def draw(data, **kwargs):
-    ax = plt.gca()
+    def draw(data, **kwargs):
+        ax = plt.gca()
 
-    for strand, sub in data.groupby("strand"):
+        for strand, sub in data.groupby("strand"):
 
-    color = "red" if strand == "+" else "blue"
+            color = "red" if strand == "+" else "blue"
 
-    ax.bar(
-        sub["Start"],
-        sub["mod"],
-        width=WINDOW_SIZE,
-        align="edge",
-        color=color
-    )
+            ax.bar(
+                sub["Start"],
+                sub["mod",
+                width=WINDOW_SIZE,
+                align="edge",
+                color=color
+            )
 
-    ax.axhline(0, color="black", linewidth=0.8)
-    ax.tick_params(axis="x", labelbottom=False)
+        ax.axhline(0, color="black", linewidth=0.8)
+        ax.tick_params(axis="x", labelbottom=False)
 
-g.map_dataframe(draw)
+    g.map_dataframe(draw)
 
-g.set_axis_labels("Genomic Position (kb)", ylab)
-g.set_titles(col_template="{col_name}", row_template="")
+    g.set_axis_labels("Genomic Position (kb)", ylab)
+    g.set_titles(col_template="{col_name}", row_template="")
 
-plt.tight_layout()
-plt.savefig(outpath, dpi=300)
-plt.close()
+    plt.tight_layout()
+    plt.savefig(outpath, dpi=300)
+    plt.close()
