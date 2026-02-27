2dc5dc110a6f543053ff2531a5902d9c90bb45eb
Mark Tan <117105171+TMarkKI@users.noreply.github.com>
2026-02-27 11:48:58 +0000

    Update figure3cd.py

--- BEDROCK_main/figures/figure3cd.py
+++ BEDROCK_main/figures/figure3cd.py
@@ -1,5 +1,6 @@
 #modified bases by AT or CG in 1kb
 import pandas as pd
+import numpy as np
 import pyranges as pr
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -8,51 +9,106 @@ from utils import make_palette
 
 WINDOW_SIZE = 1000
 
-def make_windows(df_windows):
-    assert {"Chromosome", "start", "end"}.issubset(df_windows.columns)
-    return pr.PyRanges(
-        df_windows.rename(columns={
-            "start": "Start",
-            "end": "End"
-        })
+def get_chromosome_sizes(bed_df):
+
+    chrom_sizes = (
+        bed_df
+        .groupby("Chromosome")["End_chrom_pos"]
+        .max()
+        .reset_index()
+        .rename(columns={"End_chrom_pos": "chrom_length"})
     )
+    
+    return chrom_sizes
+
+def make_1kb_windows(chrom_sizes, window_size = 1000):
+    windows = []
+    for _, row in chrom_sizes.iterrows():
+        chrom = row["Chromosome"]
+        length = row["chrom_length"]
+
+        starts = np.arrange(0, length, window_size)
+        ends = starts + window_size
+        ends[ends > length] = length
+
+        df = pd.DataFrame({
+            "Chromosome": chrom,
+            "Start": starts,
+            "End": ends
+        })
+
+        windows.append(df)
+
+    windows_df = pd.concat(windows, ignore_index=True)
+
+    return pr.Pyranges(windows_df)
 
 def bed_to_ranges(df_bed):
-    df = df_bed.copy()
-    return pr.PyRanges(
-        df_bed.rename(columns={
-            "Start_chrom_pos": "Start",
-            "End_chrom_pos": "End",
-            "sample_name": "sample"
-        })
-    )
+    df = df_bed.rename(columns={
+        "Start_chrom_pos": "Start",
+        "End_chrom_pos": "End"
+    })
+    
+    return pr.PyRanges(df)
 
-def summarize_modifications(bed_df, window_df, mod_codes):
-    bed = bed_to_ranges(bed_df)
-    windows = make_windows(window_df)
 
-    overlaps = bed.join(windows)
+def summarize_modifications(bed_df, window_pr, mod_codes):
+
+    filtered = bed_df[(bed_df["mod_code"].isin(mod_codes)) & (bed_df["mod"] == 1)].copy()
+    bed = bed_to_ranges(filtered)
+    overlaps = bed.join(windows_pr)
     df = overlaps.df
-
-    df = df[
-        df["mod_code"].isin(mod_codes) &
-        (df["mod"] == 1)
-    ]
 
     summary = (
         df.groupby(
-            ["Chromosome", "Start", "End", "strand", "sample"],
-            as_index=False
+            ["Chromosome", "Start_b", "End_b", "strand", "sample"]
         )
         .size()
-        .rename(columns={"size": "mod"})
+        .reset_index(name="mod_count")
+        .rename(columns={
+            "Start_b": "window_start",
+            "End_b": "window_end"
+        })
+    )
+    
+    all_windows = window_pr.df.rename(
+        columns={"Start": "window_start", "End": "window_end"}
+        )
+
+    samples = bed_df["sample"].unique()
+    strands = ["+", "-"]
+
+    template = (
+        all_windows
+        .assign(key=1)
+        .merge(
+            pd.DataFrame({
+                "sample": samples,
+                "strand": strands,
+                "key": 1
+            }),
+            on="key"
+        )
+        .drop("key", axis=1)
     )
 
-    summary.loc[summary["strand"] == "-", "mod"] *= -1
+    mod_count_summary_full = template.merge(
+        summary,
+        on=["Chromosome", "window_start", "window_end", "strand", "sample"],
+        how="left"
+    )
+    mod_count_summary_full["mod_count"] = mod_count_summary_full["mod_count"].fillna(0)
 
-    return summary
+    mod_count_summary_full.loc[mod_count_summary_full["strand"] == "-", "mod_count"] *= -1
 
+    
+    return mod_count_summary_full
+    
+    
 def plot_mod_windows(df, outpath, ylab):
+
+    print(df.columns)
+    print(df["mod_count"].abs().max())
 
     df = df.copy()
 
@@ -80,15 +136,16 @@ def plot_mod_windows(df, outpath, ylab):
             color = "red" if strand == "+" else "blue"
 
             ax.bar(
-                sub["Start"],
-                sub["mod"],
+                sub["window_start"],
+                sub["mod_count"],
                 width=WINDOW_SIZE,
                 align="edge",
                 color=color
             )
 
         ax.axhline(0, color="black", linewidth=0.8)
-        ax.tick_params(axis="x", labelbottom=False)
+        ax.set_xticks(ax.get_xticks())
+        ax.set_xticklabels((ax.get_xticks() / 1000).astype(int))
 
     g.map_dataframe(draw)
 
