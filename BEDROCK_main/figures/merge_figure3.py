4f800505828dbcfdf151829aa553ea4f2850643a
Mark Tan <117105171+TMarkKI@users.noreply.github.com>
2026-02-27 12:05:37 +0000

    Update merge_figure3.py

--- BEDROCK_main/figures/merge_figure3.py
+++ BEDROCK_main/figures/merge_figure3.py
@@ -1,5 +1,5 @@
 from figures.figure3ab import window_base_counts, plot_window_counts
-from figures.figure3cd import summarize_modifications, plot_mod_windows
+from figures.figure3cd import summarize_modifications, plot_mod_windows, get_chromosome_sizes, make_1kb_windows
 
 
 def run_figure3(ref_fasta, bed_all, chr_map, outdir):
@@ -12,8 +12,11 @@ def run_figure3(ref_fasta, bed_all, chr_map, outdir):
     plot_window_counts(at_df, outdir / "figure3b.tiff", "AT Count")
 
     # ---- FIGURE 3C/D ----
-    cg_mods = summarize_modifications(bed_ranges, cg_df, mod_codes=["h", "m"])
+    chrom_sizes = get_chromosome_sizes(bed_all)
+    window_pr = make_1kb_windows(chrom_sizes, window_size=1000)
+    
+    cg_mods = summarize_modifications(bed_all, window_pr, mod_codes=["h", "m"])
     plot_mod_windows(cg_mods, outdir / "figure3c.tiff", "Modified Cytosine Count")
 
-    at_mods = summarize_modifications(bed_ranges, at_df, mod_codes=["a"])
+    at_mods = summarize_modifications(bed_all, window_pr, mod_codes=["a"])
     plot_mod_windows(at_mods, outdir / "figure3d.tiff", "Modified Adenosine Count")
