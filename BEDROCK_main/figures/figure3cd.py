b479596f29a1dc9704595d2aa6e99b5e7c59cc01
Mark Tan <117105171+TMarkKI@users.noreply.github.com>
2026-03-04 17:27:28 +0000

    Update figure3cd.py

--- BEDROCK_main/figures/figure3cd.py
+++ BEDROCK_main/figures/figure3cd.py
@@ -94,7 +94,7 @@ def summarize_modifications(bed_df, window_pr, mod_codes):
 
     template = all_windows.merge(
             pd.MultiIndex.from_product(
-                [samples, strand],
+                [samples, strands],
                 names=["sample", "strand"]
             ).to_frame(index=False),
             how="cross"
